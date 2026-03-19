5bbc489456d485d1238f60f7bceeebf960a07eb8
salmansahed <salmansahed10@gmail.com>
2026-03-20 01:05:42 +0600

    add call history section and implement responsive design

--- src/components/EmergencyInfoCard.jsx
+++ src/components/EmergencyInfoCard.jsx
@@ -5,13 +5,21 @@ const EmergencyInfoCard = ({emergencyInfoPromise}) => {
     const emergencyInfoData = use(emergencyInfoPromise)
     console.log(emergencyInfoData);
     return (
-      <div className="grid grid-cols-4 gap-6 w-11/12 mx-auto mt-6">
-        {emergencyInfoData.map((infoCard) => (
-          <SingleEmergencyInfoCard
-            key={infoCard.id}
-            infoCard={infoCard}
-          ></SingleEmergencyInfoCard>
-        ))}
+      <div className="flex flex-col md:flex-row justify-between w-11/12 mx-auto my-12 gap-12">
+        <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-6 w-full md:w-[55%] lg:w-[65%] xl:w-[75%]">
+          {emergencyInfoData.map((infoCard) => (
+            <SingleEmergencyInfoCard
+              key={infoCard.id}
+              infoCard={infoCard}
+            ></SingleEmergencyInfoCard>
+          ))}
+        </div>
+        <div className="bg-green-300 w-full md:w-[45%] lg:w-[35%] xl:w-[25%] rounded">
+          <div className='flex justify-between items-center px-5 my-4'>
+            <p className='font-semibold'><i class="fa-regular fa-clock"></i> Call History</p>
+            <button className='bg-blue-600 text-white px-6 cursor-grab py-1.5 rounded-full shadow-lg hover:rounded'><i class="fa-solid fa-eraser"></i> Clear</button>
+          </div>
+        </div>
       </div>
     );
 };
